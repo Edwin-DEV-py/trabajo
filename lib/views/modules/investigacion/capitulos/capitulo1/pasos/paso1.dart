6ef1b1b7be9a57bc833d85e33fd2eb110a12ec46
edwin-tarazona <80715680+Edwin-DEV-py@users.noreply.github.com>
2023-11-15 20:49:00 -0500

    listo el contenido

--- lib/views/modules/investigacion/capitulos/capitulo1/pasos/paso1.dart
+++ lib/views/modules/investigacion/capitulos/capitulo1/pasos/paso1.dart
@@ -1,69 +1,52 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:trabajo/widgets/contenido.dart';
 import 'package:trabajo/widgets/header.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class cap1_Paso1 extends StatelessWidget {
   const cap1_Paso1({super.key});
 
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Column(
-//contenedor del header
         children: [
           HeaderContainer3(txt: '1'),
-//contenedor de contenido
           Container(
-            child: SingleChildScrollView(
-              child: Container(
+            child: Container(
                 width: 360,
-                child: Column(
-                    children: [
+                height: screenHeight*0.86,
+                child: ListView(
+                  children: [
+                    Widget_titulo('SELECCION DE TEMA'),
                     SizedBox(height: 10,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Title(color: Colors.black, child: Text('SELECCION DE TEMA', style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold
-                        ),)),
-                      ],
-                    ),
+                    Widget_texto('Para esta actividad se hace necesario ver el video y buscar temas de su interés para dar ejemplo de temáticas, elijan un tema de su interés para investigar debe ser sencillo, que conozcas, de tu gusto, que creas que puede llevar a cabo y que esté a tu disponibilidad. Con base en el video, realiza un mapa conceptual sobre los pasos para hacer una investigación, selecciona un tema relacionado con tus futuros estudios universitarios o con tus intereses. Posteriormente, se te facilitará un tema como propuesta y con él, trabajarás entre todas las siguientes etapas de una investigación. A esta temática se le debe reducir, especificar, delimitar en tiempo y lugar.',),
                     SizedBox(height: 10,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Expanded(
-                          child: Text('Para esta actividad se hace necesario ver el video y buscar temas de su interés para dar ejemplo de temáticas, elijan un tema de su interés para investigar debe ser sencillos que conozcas de su gusto que creas que puede llevar a cabo y que este a su disponibilidad. Con base en el video realiza un mapa conceptual sobre los pasos para hacer una investigación, selecciona un tema relacionado con sus futuros estudios universitarios o con sus intereses, posteriormente se les facilita un tema como propuesta y con él, se trabajará entre todas las siguientes etapas de una investigación. A esta temática se le debe reducir, especificar, delimitar en tiempo, lugar.', 
-                            textAlign: TextAlign.justify,
-                          )
-                        )
-                      ],
-                    ),
+                    Widget_url('https://www.youtube.com/watch?v=Wx5UhVHthVw', 'paso1.1.jpg'),
+                    SizedBox(height: 30,),
+                    Widget_titulo('BUSQUEDA DE FUENTES'),
                     SizedBox(height: 10,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        YoutubePlayer(
-                          controller: YoutubePlayerController(
-                            initialVideoId: 'https://www.youtube.com/watch?v=Wx5UhVHthVw',
-                            flags: YoutubePlayerFlags(
-                              autoPlay: false,
-                            )
-                          ),
-                          showVideoProgressIndicator: true,
-                        )
-                      ],
-                    )
+                    Widget_texto('Utiliza las bases de datos académicas, bibliotecas digitales y otras fuentes confiables.'),
+                    Widget_texto('Proporciónales una lista de palabras clave relacionadas con su tema para facilitar la búsqueda.'),
+                    SizedBox(height: 10,),
+                    Widget_Subtitulo('Fuentes de Información:'),
+                    Widget_texto('Consulta cuales son las fuentes de información primarias, segundarias y terciarias con ellas realiza una infografía en las planillas de Canva.'),
+                    Widget_texto('Reglas básicas de citación y referencia en un estilo específico (APA, MLA, etc.).'),
+                    Widget_texto('Solicita que practiquen la correcta citación de sus fuentes.'),
+                    SizedBox(height: 10,),
+                    Widget_url('https://www.youtube.com/watch?v=Wx5UhVHthVw', 'paso1.1.jpg'),
+                    SizedBox(height: 30,),
+                    Widget_herramienta_ayuda(),
+                    SizedBox(height: 30,),
                   ],
                 ),
               ),
             ),
-          )
         ],
       ),
     );
   }
 }
+
---
+++ lib/widgets/contenido.dart
@@ -0,0 +1,159 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+Widget Widget_titulo(String txt){
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Title(color: Colors.black, child: Text(txt, style: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.bold
+      ),)),
+    ],
+  );
+}
+
+Widget Widget_Subtitulo(String txt){
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.start,
+    children: [
+      Title(color: Colors.black, child: Text(txt, style: TextStyle(
+        fontSize: 15,
+        fontWeight: FontWeight.bold
+      ),)),
+    ],
+  );
+}
+
+Widget Widget_texto(String txt){
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Expanded(
+        child: Text(
+          txt,
+          textAlign: TextAlign.justify,
+        ),
+      )
+    ],
+  );
+}
+
+Widget Widget_url(String txt, String img){
+
+  _launchURL(String url) async {
+  if (!await launchUrl(Uri.parse(url))) {
+    throw 'No se pudo abrir la URL: $url';
+  }else{
+      await launchUrl(Uri.parse(url));
+    }
+  }
+
+  return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        GestureDetector(
+          onTap: () {
+            _launchURL(txt);
+          },
+          child: Image(
+            image: AssetImage('assets/miniaturas/$img'),
+            fit: BoxFit.cover,
+            width: 350,
+            height: 200,
+          ),
+        ),
+      ],
+    );
+}
+
+Widget Widget_herramienta_ayuda(){
+  return Column(
+    children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Title(color: Colors.black, child: Text('HERRAMIENTAS DE AYUDA', style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold
+          ),)),
+        ],
+      ),
+      SizedBox(height: 10,),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Text(
+              'Con estas herramientas, tienes la capacidad de formular preguntas específicas sobre los temas que te resulten más desafiantes, y recibir respuestas generadas por un motor de inteligencia artificial.',
+              textAlign: TextAlign.justify,
+            ),
+          )
+        ],
+      ),
+      SizedBox(height: 10,),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image(
+            image: AssetImage('assets/gt.png'),
+            width: 100,
+          ),
+          Image(
+            image: AssetImage('assets/bing.png'),
+            width: 100,
+          )
+        ],
+      ),
+      SizedBox(height: 10,),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Text(
+              'Con esta herramienta, podrás plantearle preguntas a tu docente acerca de cualquier tema que hayas abordado.',
+              textAlign: TextAlign.justify,
+            ),
+          )
+        ],
+      ),
+      SizedBox(height: 10,),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image(
+            image: AssetImage('assets/what.webp'),
+            width: 120,
+          ),
+        ],
+      ),
+      SizedBox(height: 10,),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Text(
+              'Con esta herramienta, podrás realizar búsquedas en internet para consultar información adicional sobre diversos temas o resolver cualquier inquietud que puedas tener.',
+              textAlign: TextAlign.justify,
+            ),
+          )
+        ],
+      ),
+      SizedBox(height: 10,),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image(
+            image: AssetImage('assets/gg.png'),
+            width: 100,
+          ),
+        ],
+      ),
+    ]
+    
+  );
+}
+
+
